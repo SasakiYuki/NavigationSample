56a5948f2282c6eca8ef61f51958460915d9f7e4
SasakiYuki <wacorggen@gmail.com>
2018-11-30 11:24:17 +0900

    🛠 LogicFix supportLibrary -> androidx

--- app/src/main/java/sample/android/jp/yamada/yuki/navigationsampleapp/MainActivity.kt
+++ app/src/main/java/sample/android/jp/yamada/yuki/navigationsampleapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package sample.android.jp.yamada.yuki.navigationsampleapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
